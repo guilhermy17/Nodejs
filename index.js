32d5cac9c68c8c504b0d9dc8ddcbde09f20b5f87
Guilhermy <133382489+guilhermy17@users.noreply.github.com>
2024-09-02 13:13:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const idade = 28;
+const nome = "Guilhermy";
+const sobrenome = "Ferreira";
+
+console.log(nome+""+ sobrenome);
+console.log(nome, sobrenome);
+
+console.log('Meu nome é ${nome) e meu sobrenome é ${sobrenome}');
+
+
+// NaN Not a Number
